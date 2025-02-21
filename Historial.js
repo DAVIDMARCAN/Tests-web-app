f9b71df0807e109600985f2a7bf273846127a966
DAVIDMARCAN <93211434+DAVIDMARCAN@users.noreply.github.com>
2025-02-21 14:47:01 -0600

    Update Historial.js

--- Historial.js
+++ Historial.js
@@ -178,16 +178,6 @@ function filtrarHistorial() {
     }).catch(error => console.error("Error obteniendo historial filtrado:", error));
 }
 
-// Verificar si hay una sesión activa como admin
-if (localStorage.getItem("admin") !== "true") {
-    alert("Debes iniciar sesión como administrador.");
-    window.location.href = "https://acceso-qr.vercel.app/index.html"; // Redirigir a la página de inicio de sesión
-}
-
-window.cerrarSesion = function () {
-    localStorage.removeItem("admin"); // Eliminar sesión
-    window.location.href = "https://acceso-qr.vercel.app/index.html"; // Redirigir a la página de inicio de sesión
-};
 
 
 cargarTrabajadores();
